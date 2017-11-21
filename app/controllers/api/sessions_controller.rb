2379db7d3f482937dade0cf7f427310929f991dd
Chi Yang <nunuh89@gmail.com>
2017-11-21 14:05:38 -0800

    add auth methods to controllers

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,32 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if @user
+      signin(@user)
+      render "api/users/show"
+    else
+      render(
+        json: { base: ["Credentials are incorrect"] },
+        status: 401
+      )
+    end
+  end
+
+  def destroy
+    @user = current_user
+
+    if @user
+      signout
+      render "api/users/show"
+    else
+      render(
+        json: { destroySession: ["Not Signed In"] },
+        status: 404
+      )
+    end
+  end
 end
